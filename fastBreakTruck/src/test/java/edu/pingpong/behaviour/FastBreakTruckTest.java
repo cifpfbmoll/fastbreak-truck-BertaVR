4771b245c3d48a7c4043807cdfac7888860bc164
BertaVR <bverges@cifpfbmoll.eu>
2021-05-02 23:22:45 +0200

    style:  formato, ahora sí que sí, proyecto (espero que) acabado

--- fastBreakTruck/src/test/java/edu/pingpong/behaviour/FastBreakTruckTest.java
+++ fastBreakTruck/src/test/java/edu/pingpong/behaviour/FastBreakTruckTest.java
@@ -2,30 +2,29 @@ package edu.pingpong.behaviour;
 
 import edu.pingpong.domain.*;
 import org.junit.Test;
-import org.junit.experimental.theories.suppliers.TestedOn;
 
 import static org.junit.Assert.*;
 
 public class FastBreakTruckTest {
 
     @Test
-    public void eyeHolesTest(){
-        assertEquals(2,FastBreakTruck.prepararEyeHoles().getItems().size());
+    public void eyeHolesTest() {
+        assertEquals(2, FastBreakTruck.prepararEyeHoles().getItems().size());
         assertTrue(FastBreakTruck.prepararEyeHoles().getItems().stream().anyMatch(i -> i instanceof Eyeholes));
         assertTrue(FastBreakTruck.prepararEyeHoles().getItems().stream().anyMatch(i -> i instanceof TurbulentJuice));
 
     }
 
     @Test
-    public void smigglesTest(){
-        assertEquals(2,FastBreakTruck.prepararSmiggles().getItems().size());
+    public void smigglesTest() {
+        assertEquals(2, FastBreakTruck.prepararSmiggles().getItems().size());
         assertTrue(FastBreakTruck.prepararSmiggles().getItems().stream().anyMatch(i -> i instanceof Smiggles));
         assertTrue(FastBreakTruck.prepararSmiggles().getItems().stream().anyMatch(i -> i instanceof FleebJuice));
 
     }
 
     @Test
-    public void incluirJugueteTest(){
+    public void incluirJugueteTest() {
         assertFalse(FastBreakTruck.incluirJuguete(FastBreakTruck.prepararSmiggles()));
         assertTrue(FastBreakTruck.incluirJuguete(FastBreakTruck.prepararEyeHoles()));
 
--- fastBreakTruck/src/test/java/edu/pingpong/domain/DesayunoTest.java
+++ fastBreakTruck/src/test/java/edu/pingpong/domain/DesayunoTest.java
@@ -1,6 +1,6 @@
 package edu.pingpong.domain;
+
 import edu.pingpong.behaviour.FastBreakTruck;
-import edu.pingpong.domain.Desayuno;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,16 +8,15 @@ import static org.junit.Assert.assertEquals;
 public class DesayunoTest {
 
     @Test
-    public void desayunoTest(){
+    public void desayunoTest() {
         Desayuno smigDes1 = FastBreakTruck.prepararSmiggles();
-        assertEquals(85f, smigDes1.getCoste(),  0.000);
+        assertEquals(85f, smigDes1.getCoste(), 0.000);
         Desayuno eyeDes1 = FastBreakTruck.prepararEyeHoles();
-        assertEquals(55f, eyeDes1.getCoste(),  0.000);
+        assertEquals(55f, eyeDes1.getCoste(), 0.000);
         FastBreakTruck.incluirJuguete(eyeDes1);
-        assertEquals(155f, eyeDes1.getCoste(),  0.000);
+        assertEquals(155f, eyeDes1.getCoste(), 0.000);
         FastBreakTruck.incluirJuguete(smigDes1);//precio no cambia porque es smiggles y no se tiene que poder añadir
-        assertEquals(85f, smigDes1.getCoste(),  0.000);
-
+        assertEquals(85f, smigDes1.getCoste(), 0.000);
 
 
     }
