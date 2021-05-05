bc1bc71fb299720aa68cf915693cbd09eae34030
Berta Vergés <72273706+BertaVR@users.noreply.github.com>
2021-05-05 23:23:07 +0200

    Delete fastBreakTruck directory

--- fastBreakTruck/src/main/java/edu/pingpong/behaviour/Item.java
+++
@@ -1,9 +0,0 @@
-package edu.pingpong.behaviour;
-
-public interface Item {
-    String nombre();
-
-    Packing empaquetado();
-
-    Float pvp();
-}
--- fastBreakTruck/src/main/java/edu/pingpong/behaviour/Packing.java
+++
@@ -1,5 +0,0 @@
-package edu.pingpong.behaviour;
-
-public interface Packing {
-    String envoltorio();
-}
